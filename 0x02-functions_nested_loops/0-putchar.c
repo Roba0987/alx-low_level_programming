fed8526d37294cae8a908f574fa5c6d7f028e2c0
Roba0987 <robstarbekele098@gmail.com>
2022-09-13 10:32:30 -0700

    project

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
  * main -  prints Holberton, followed by a new line
@@ -6,6 +7,7 @@
  */
 int main(void)
 {
+	_putchar('_');
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');
--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
  * main - finds and prints the sum of the even-valued terms
