e5bf7875403c5b63e20472a3c6f623c3671998a0
Roba0987 <robstarbekele098@gmail.com>
2022-09-13 08:11:36 -0700

    project

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main -  prints Holberton, followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('a');
+	_putchar('r');
+
+	return (0);
+} 
