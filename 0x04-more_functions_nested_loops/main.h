77f75825e437e360a5db6ef783a38b44f5b79823
Roba0987 <robstarbekele098@gmail.com>
2022-09-15 14:32:04 -0700

    project

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,3 +1,4 @@
+#ifndef main_h;
 int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
@@ -10,6 +11,5 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
-#endif
-#ifndef FILE_HOLBERTON
-#define FILE_HOLBERTON
+#endif;
+#define main_h;
